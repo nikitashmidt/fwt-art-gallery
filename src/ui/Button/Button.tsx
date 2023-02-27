a4cf756fb580d725e83ed5dd7d2f9e91748f2769
Nikita <nikitka.shmidt.01@mail.ru>
2023-02-27 13:49:20 +0400

    fixed bugs

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,26 +1,40 @@
-import cn  from 'classnames'
+import cn from 'classnames';
 import './style.scss';
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    onClick?: () => void;
-    isDark?: boolean,
-    isOutlined?: boolean,
-    isDisabled?: boolean,
-    className: 'button__default' | 'button__text' | 'button__trash' | 'button__theme' | 'button__circle',
-    buttonOverImage?: boolean,
-}
-export const Button = ({onClick, isDark, isOutlined, isDisabled, children, className, buttonOverImage, }:IButtonProps) => {
-    const dark = isDark && `${className}--dark`;
-    const outlined = isOutlined && `${className}--outlined`;
-    const btnOverImage = buttonOverImage && `${className}--boi`;
-    return(
-        <button 
-            className={cn('button', className, dark, outlined, btnOverImage )}
-            disabled={isDisabled}
-            onClick={onClick}
-        >
-            {children}
-        </button>
-    )
+	onClick?: () => void;
+	isDark: boolean;
+	isOutlined?: boolean;
+	isDisabled?: boolean;
+	className:
+		| 'button__default'
+		| 'button__text'
+		| 'button__trash'
+		| 'button__theme'
+		| 'button__circle';
+	isBtnOverImage?: boolean;
 }
 
+export const Button = ({
+	onClick,
+	isDark,
+	isOutlined,
+	isDisabled,
+	children,
+	className,
+	isBtnOverImage,
+}: IButtonProps) => {
+	return (
+		<button
+			className={cn('button', 
+            {[`${className}--dark`]: isDark }, 
+            {[`${className}--outlined`]: isOutlined},
+            {[`${className}--boi`]: isBtnOverImage},
+            {[`${className}--disabled`]: isDisabled},
+            className)}
+			disabled={isDisabled}
+			onClick={onClick}>
+			{children}
+		</button>
+	);
+};
--- src/ui/Grid/Grid.tsx
+++ src/ui/Grid/Grid.tsx
@@ -1,25 +1,26 @@
-import useTheme from '../../hooks/useTheme';
-import { newGuid } from '../../utils/guid'; 
-import cn  from 'classnames';
+import { newGuid } from '../../utils/guid';
+import cn from 'classnames';
 import Card from '../Card';
 import './style.scss';
 
 interface IPropsGrid {
-  artists: any[]
+	artists: any[];
 }
-const Grid = ({artists}:IPropsGrid) => {
-  const { isDark } = useTheme();
-  
-  return (
-    <ul className={cn('grid')}>
-        {artists.map((artist)=> {
-          return (
-            <Card key={newGuid()} isDark={isDark} thumbnail={artist.thumbnail} name={artist.name} 
-              dateFirst={artist.dateFirst} dateSecond={artist.dateSecond}
-            />
-          )
-        })}
-    </ul>
-  );
+const Grid = ({ artists }: IPropsGrid) => {
+	return (
+		<ul className={cn('grid')}>
+			{artists.map((artist) => {
+				return (
+					<Card
+						key={newGuid()}
+						thumbnail={artist.thumbnail}
+						name={artist.name}
+						dateFirst={artist.dateFirst}
+						dateSecond={artist.dateSecond}
+					/>
+				);
+			})}
+		</ul>
+	);
 };
 export default Grid;
